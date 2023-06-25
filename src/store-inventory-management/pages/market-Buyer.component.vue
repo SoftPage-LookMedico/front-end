ac29a512c3d23b46735b97a1fb00ff2f7d92a9ea
Rodrigo-Hidalgo-Nicho <89040394+Rodrigo-Hidalgo-Nicho@users.noreply.github.com>
2023-06-25 17:28:48 -0500

    feat: Update market-Buyer.component.vue

--- src/store-inventory-management/pages/market-Buyer.component.vue
+++ src/store-inventory-management/pages/market-Buyer.component.vue
@@ -1,9 +1,5 @@
 <template>
 
-  <!--buttons addCaterory + Inventory-->
-
-  <!--/buttons addCaterory + Inventory-->
-
   <!--products load-->
 
     <div class="font-bold flex align-items-center justify-content-center p-4 border-round">
@@ -20,16 +16,13 @@
             <template #list="slotProps">
                 <div class="col-12">
                     <div class="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
-                        <img class="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" :src="`https://norvil-web.storage.googleapis.com/cms_multimedia/cms_medias/files/000/002/134/tablet_webp/1739_593.c141.webp?1615459779`" :alt="slotProps.data.name" />
+                        <img class="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" :src="`${slotProps.data.photoUrl}`" :alt="slotProps.data.photoUrl"/>
                         <div class="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
                             <div class="flex flex-column align-items-center sm:align-items-start gap-3">
-                                <div class="text-2xl font-bold">{{ slotProps.data.title }}</div>
+                                <router-link :to="{name:'Product-selection', params:{id : products.title}}" >
+                                    <div class="text-2xl font-bold">{{ slotProps.data.title }}</div>
+                                </router-link>
                                 <div class="text-2xl ">{{ slotProps.data.description }}</div>
-                                <pv-rating :modelValue="slotProps.data.rating" readonly :cancel="false">
-                                    <div class="card flex justify-content-center">
-                                        <Rating v-model="value" :cancel="false"  />
-                                    </div>
-                                </pv-rating>
                                 <div class="flex align-items-center gap-3">
                                     <span class="flex align-items-center gap-2">
                                         <i class="pi pi-tag"></i>
@@ -40,6 +33,20 @@
                             </div>
                             <div class="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
                                 <span class="text-2xl font-semibold">${{ slotProps.data.price }}</span>
+                                <router-link
+                                    to="/product-selection"
+                                    custom
+                                    v-slot="{ navigate }"
+                                >
+                                    <pv-button
+                                        @click="navigate"
+                                        role="link"
+                                        v-bind:class="{'lightblue': !clicked, 'green': clicked}"
+                                        v-on:click ="clicked = !clicked"
+                                    >
+                                        Select
+                                    </pv-button>
+                                </router-link>
                                 <pv-button icon="pi pi-shopping-cart" rounded :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"></pv-button>
                             </div>
                         </div>
@@ -58,18 +65,30 @@
                             <pv-tag :value="slotProps.data.inventoryStatus" :severity="getSeverity(slotProps.data)"></pv-tag>
                         </div>
                         <div class="flex flex-column align-items-center gap-3 py-5">
-                            <img class="w-9 shadow-2 border-round" :src="`https://norvil-web.storage.googleapis.com/cms_multimedia/cms_medias/files/000/002/134/tablet_webp/1739_593.c141.webp?1615459779`" :alt="slotProps.data.name" @click="navigate" v-on:click ="clicked = !clicked" />
-                            <div class="text-2xl font-bold">{{ slotProps.data.title }}</div>
+                            <img class="w-9 shadow-2 border-round" :src="`${slotProps.data.photoUrl}`" :alt="slotProps.data.photoUrl" @click="navigate" v-on:click ="clicked = !clicked" />
+                            <router-link :to="{name:'Product-selection', params:{id : slotProps.data.id}}" >
+                                <div class="text-2xl font-bold">{{ slotProps.data.title }}</div>
+                            </router-link>
+
                             <div class="text-2xl ">{{ slotProps.data.description }}</div>
-                            <div class="card flex justify-content-center">
-                                <Rating v-model="value" :cancel="false"  />
-                            </div>
                         </div>
                         <div class="flex align-items-center justify-content-between">
                             <span class="text-2xl font-semibold">${{ slotProps.data.price }}</span>
-                            <pv-button icon="pi pi-shopping-cart" rounded :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"
-                            v-if="!products.cart" @click="products.cart=true, addtocart(JSON.parse(JSON.stringify(products)))" block href="#"
-                            variant="success"></pv-button>
+                            <router-link
+                                to="/product-selection"
+                                custom
+                                v-slot="{ navigate }"
+                            >
+                                <pv-button
+                                    @click="navigate"
+                                    role="link"
+                                    v-bind:class="{'lightblue': !clicked, 'green': clicked}"
+                                    v-on:click ="clicked = !clicked"
+                                >
+                                    Select
+                                </pv-button>
+                            </router-link>
+                            <pv-button @click="addToCart(products.title, i)" icon="pi pi-shopping-cart" rounded :disabled="slotProps.data.inventoryStatus === 'OUTOFSTOCK'"></pv-button>
                         </div>
                     </div>
                 </div>
@@ -85,18 +104,18 @@
 <script>
 import { ProductsApiService } from '../services/products-api.service.js';
 
-export default {
+export default {Item: null,
     data() {
         return {
             cart: [],
             selectedItem: null,
-            items: Array.from({ length: 30 }, (_, i) => ({ label: `Item #${i+1}`, value: i })),
             products: {},
             layout: 'grid',
             productsService: null,
             value: null
         };
     },
+    props: ['inventory', 'addToCart'],
     created() {
         console.log("created");
         this.productsService = new ProductsApiService();
@@ -109,16 +128,11 @@ export default {
     },
 
     methods: {
-        addtocart(product){
-          let cart=[]
-          if (JSON.parse(localstorage.getItem('products'))) {
-              cart = JSON.parse(localstorage.getItem('products'))
-          }  else {
-              cart.push(product);
-              localStorage.setItem('products',JSON.stringify(cart))
-          }
+        addToCart (title, index) {
+          if(!this.cart[title]) this.cart[title] = 0
+            this.cart[title] += this.inventory[index].quantity
+            this.inventory[index].quantity = 0
         },
-
         getSeverity(product) {
             switch (product.inventoryStatus) {
                 case 'INSTOCK':
